6b5e824f068af0b9952b75c7f2c6c087254b885b
adri_ <adri_@Asriano-Lenovo>
2024-05-13 10:53:40 +0200

    mi primer commit 13.05

---
+++ src/proyectoGitHub/primeraclase.java
@@ -0,0 +1,11 @@
+package proyectoGitHub;
+
+public class primeraclase {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Primer mensaje de prueba hoy");
+
+	}
+
+}
