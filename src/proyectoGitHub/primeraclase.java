dab971668cde7a1433a899d6973da3ff802e5e8c
adri_ <adri_@Asriano-Lenovo>
2024-05-13 13:31:31 +0200

    nuevo commit

--- src/proyectoGitHub/primeraclase.java
+++ src/proyectoGitHub/primeraclase.java
@@ -5,7 +5,8 @@ public class primeraclase {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Primer mensaje de prueba hoy");
-		System.out.println("Mensaje creado desde GitHub")
+		System.out.println("Mensaje creado desde GitHub");
+		System.out.println("mensaje tras clonacion");
 
 	}
 
