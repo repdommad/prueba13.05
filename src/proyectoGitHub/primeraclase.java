00e98c6278b7de494a09ebe1cefea1c73f49c561
adrianosantanadelacruz <adridelacruzsantana@gmail.com>
2024-05-13 12:35:44 +0200

    Update primeraclase.java
    Agreado un nuevo comentario para imprimir por pantaya

--- src/proyectoGitHub/primeraclase.java
+++ src/proyectoGitHub/primeraclase.java
@@ -5,6 +5,7 @@ public class primeraclase {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Primer mensaje de prueba hoy");
+		System.out.println("Mensaje creado desde GitHub")
 
 	}
 
